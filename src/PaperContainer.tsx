25fa5c0a527ce69206078350264bcaee4e83f4de
Anton J Olsson <ao@antolsson.se>
2025-11-04 00:00:00 +0100

    style: 💄 link-button

--- src/PaperContainer.tsx
+++ src/PaperContainer.tsx
@@ -5,7 +5,7 @@ export function PaperContainer( props: { title : string, description : string, u
         <h2 className="paper-title">{props.title}</h2>
         <p className="paper-description"> {props.description} </p>
         <div className="paper-buttons">
-            <button className="button"><a href={props.url}>View PDF</a></button>
+            <button className="link-button"><a href={props.url}>View PDF</a></button>
             {/*<button className="button">More info</button>*/}
         </div>
     </div>;
