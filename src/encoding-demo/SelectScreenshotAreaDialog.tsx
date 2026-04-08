e3cd1261f6ecc7f9401b0c8924c928535f8a0cdd
Anton J Olsson <ao@antolsson.se>
2026-04-09 00:00:00 +0200

    fix: 🐛 minor fixes

--- src/encoding-demo/SelectScreenshotAreaDialog.tsx
+++ src/encoding-demo/SelectScreenshotAreaDialog.tsx
@@ -12,11 +12,11 @@ export function SelectScreenshotAreaDialog(props: {
     const getTitle = () => {
         return props.autoScroll
             ? 'Press Take Screenshot to scroll to demo start and take screenshot.'
-            : 'Scroll to select screenshot area and click the button.';
+            : 'Scroll to select screenshot area and click the Take Screenshot button.';
     }
     return <Dialog show={props.show} title={getTitle()}
                    setHide={props.onCancel} blurBackground={props.blurBackground}
-                   allowScroll={true} className={'select-screenshot-area-dialog'}>
+                   allowScroll={!props.autoScroll} className={'select-screenshot-area-dialog'}>
         <>
             <p>When selecting capture source, choose this window (not tab), for optimal cropping.</p>
             <p>You might have to give your browser permission for screen captures.</p>
