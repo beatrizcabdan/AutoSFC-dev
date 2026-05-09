2be68f0de6deff6a3a20af0d8647a9e0da1c2391
Anton J Olsson <ao@antolsson.se>
2026-05-10 00:00:00 +0200

    feat: 🎸 add p to LoadRemoteFileDialog

--- src/encoding-demo/LoadRemoteFileDialog.tsx
+++ src/encoding-demo/LoadRemoteFileDialog.tsx
@@ -19,6 +19,7 @@ export function LoadRemoteFileDialog(props: {
 
     return <Dialog show={props.show} title={'Insert URL of remote file to load'} setHide={props.hide} className={'load-remote-file-dialog'}>
         <>
+            <p>File content hash may be appended as search parameter as well.</p>
             <TextField variant={'filled'} size={'medium'} onChange={e => setCurrentUrl(e.target.value)}
                        onKeyDown={e => onKeyDown(e)}
                        placeholder={'https://wwww.example.com/example-data.csv&contentHash=bcc82e3a4b6877e31cbc5ca142b7873b'}
