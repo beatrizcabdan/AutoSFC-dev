ee1dafa2e675651e9cb8edece0786edcb845cc1b
Anton J Olsson <ao@antolsson.se>
2026-05-10 00:00:00 +0200

    feat: 🎸 validate url in dialog

--- src/encoding-demo/LoadRemoteFileDialog.tsx
+++ src/encoding-demo/LoadRemoteFileDialog.tsx
@@ -17,15 +17,25 @@ export function LoadRemoteFileDialog(props: {
         }
     }
 
+    function inputIsCorrect() {
+        // URL
+        // TODO: contentHash only allowed search param, remove input when hiding dialog
+        return currentUrl === '' || !!currentUrl.match(/(?:http[s]?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@%._+~#=]{2,256}\.[a-z]{2,6}\b[-a-zA-Z0-9@:%_+.~#?&\/=]*/)
+    }
+
     return <Dialog show={props.show} title={'Insert URL of remote file to load'} setHide={props.hide} className={'load-remote-file-dialog'}>
         <>
             <p>File content hash may be appended as search parameter as well.</p>
             <TextField variant={'filled'} size={'medium'} onChange={e => setCurrentUrl(e.target.value)}
-                       onKeyDown={e => onKeyDown(e)}
+                       onKeyDown={e => onKeyDown(e)} error={!inputIsCorrect()}
                        placeholder={'https://wwww.example.com/example-data.csv&contentHash=bcc82e3a4b6877e31cbc5ca142b7873b'}
+                       helperText={inputIsCorrect() ? ' ' : 'Incorrect URL format.'}
                        value={currentUrl} fullWidth inputRef={input => input && input.focus()} autoComplete={'false'} autoSave={'false'}/>
             <div className={'dialog-buttons'}>
-                <Button className={'cancel-button'} onClick={props.onCancel}>Cancel</Button>
+                <Button className={'cancel-button'} onClick={() => {
+                    setCurrentUrl('');
+                    props.onCancel()
+                }}>Cancel</Button>
                 <Button className={'load-url-button'} // @ts-ignore
                         onClick={() => props.onFileChosen(currentUrl)}>Load file</Button>
             </div>
