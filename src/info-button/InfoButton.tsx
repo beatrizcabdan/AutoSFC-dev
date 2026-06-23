a2c0836482edc30dc8aac97ec509894b429f142f
Anton J Olsson <ao@antolsson.se>
2026-06-24 00:00:00 +0200

    style: 💄 make icon positions absolute

--- src/info-button/InfoButton.tsx
+++ src/info-button/InfoButton.tsx
@@ -3,12 +3,13 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import {Tooltip} from "../tooltip/Tooltip.tsx";
 import './InfoButton.scss'
 
-export function InfoButton(props: { msg: string, translateTooltip?: string }) {
+export function InfoButton(props: { msg: string, translateTooltip?: string, top?: string, left?: string }) {
     const iconRef = useRef(null)
     const [showTooltip, setShowTooltip] = useState(false)
     return <>
         <Tooltip msg={props.msg} showMsg={showTooltip} anchorRef={iconRef} translate={props.translateTooltip ?? '-50% -160%'}/>
-        <InfoOutlinedIcon fontSize={'small'} className={'info-icon'} ref={iconRef}
+        <InfoOutlinedIcon fontSize={'small'} className={'info-icon'} ref={iconRef} style={{top: props.top ?? 'initial',
+            left: props.left ?? 'initial'}}
                           onMouseOver={() => setShowTooltip(true)}
                           onMouseOut={() => setShowTooltip(false)}/>
     </>;
